f8f0ce23d42dd090d486dcab4c391c72b2b3d23b
Anton Lebedev <sorrint@gmail.com>
2022-07-11 22:13:40 +0800

    publish new repo

---
+++ src/components/counter.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+const Counter = (props) => {
+  const {value} = props;
+  //   console.log(props);
+
+  const formatValue = () => {
+    return value === 0 ? "empty" : value;
+  };
+  const getBageClasses = () => {
+    let classes = "badge m-2 ";
+    classes += value === 0 ? "bg-warning" : "bg-primary";
+    return classes;
+  };
+
+  //   const handleIncrement = () => {
+  // console.log("handle increment");
+  //   };
+  //   const handleDecrement = () => {
+  // console.log("handle decrement");
+  //   };
+
+  return (
+    <div>
+      <span> {props.name}</span>
+      <span className={getBageClasses()}>{formatValue()}</span>
+      <button
+        className="btn btn-primary btn-sm m-2"
+        onClick={() => props.onIncrement(props.id)}
+      >
+        +
+      </button>
+      <button
+        className="btn btn-primary btn-sm m-2"
+        onClick={() => props.onDecrement(props.id)}
+      >
+        -
+      </button>
+      <button
+        className="btn btn-danger btn-sm m-2"
+        onClick={() => props.onDelete(props.id)}
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
